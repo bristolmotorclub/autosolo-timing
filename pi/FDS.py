67911ab28a8e30c6d36672a3b530740001595823
bristolmotorclub <43007222+bristolmotorclub@users.noreply.github.com>
2022-06-15 18:13:43 +0100

    Added demo script to communicate with FDS timing box

---
+++ pi/FDS.py
@@ -0,0 +1,26 @@
+# Demo script to pull data from FDS timing device plugged in via USB
+
+import serial
+import datetime
+import time
+def DecodeRaw(rawtiming):
+    utftime = rawtiming[0:len(rawtiming)-2].decode("utf-8")
+    TimeSplitArray = utftime.split()
+    TriggeredInput = TimeSplitArray[2]
+    TimeString = TimeSplitArray[3]
+    TimeArray = time.strptime(TimeString.split('.')[0],'%H:%M:%S')
+    TriggeredTime = datetime.timedelta(hours=TimeArray.tm_hour,minutes=TimeArray.tm_min,seconds=TimeArray.tm_sec).total_seconds() + int(TimeString.split('.')[1])/100000
+    print(TriggeredInput,TriggeredTime)
+    return (TriggeredInput,TriggeredTime)
+def RecordTime(Inp,Tim):
+    return Inp,Tim
+def StartLight(status):
+    return status
+ser = serial.Serial('COM3',9600,timeout=10)  # open serial port
+print(ser.name)         # check which port was really used
+while 1:
+    rawtiming = ser.readline()
+    if rawtiming:
+        ThisTime = DecodeRaw(rawtiming)
+    #RecordTime(ThisTime[0],ThisTime[1])
+ser.close()             # close port
